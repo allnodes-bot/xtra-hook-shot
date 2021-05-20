b252369deebb86e07a79566f3d2307627df5707a
xtr <xtr@destiny.games>
2021-05-20 07:48:53 +0300

    experiment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
-if [ $(curl -s -o /dev/null -w "%{http_code}" -X POST $3 --data \"\$1\" $2) -ne 200 ]; then
-  echo "%{http_code}"
-  exit 1;
-fi
+
+echo $(curl -s -o /dev/null -w "%{http_code}" -X POST $3 --data \"\$1\" $2)
+
+#if [ $(curl -s -o /dev/null -w "%{http_code}" -X POST $3 --data \"\$1\" $2) -ne 200 ]; then
+#  echo "%{http_code}"
+#  exit 1;
+#fi
