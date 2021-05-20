ccbca8e142722cfab1a88cac06d6926caaa4f2c5
xtr <xtr@destiny.games>
2021-05-20 18:21:00 +0300

    Fix argument parsing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-req="curl --write-out '%{http_code}\n' --silent --output /dev/null -X POST $3 --data \"$1\" $2"
+req="curl --write-out '%{http_code}\n' --silent --output /dev/null -X POST $3 --data \"\$1\" $2"
 response_code=$(eval $req)
 
 echo "::set-output name=response_code::$response_code"
