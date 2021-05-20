55a1702085301748b426adb2f54b26adddf15958
xtr <xtr@destiny.games>
2021-05-20 08:16:38 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
 
+code=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$3" --data \"\$1\" "$2")
+echo "::set-output name=code::$code"
 
-echo $(curl -s -o /dev/null -w "%{http_code}" -X POST $3 --data \"\$1\" $2)
+
 
 #if [ $(curl -s -o /dev/null -w "%{http_code}" -X POST $3 --data \"\$1\" $2) -ne 200 ]; then
 #  echo "%{http_code}"
