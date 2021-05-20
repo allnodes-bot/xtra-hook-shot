1f9c1da782917b56452002bb4f4519cd34b819d4
xtr <xtr@destiny.games>
2021-05-20 12:33:22 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,11 @@
 #!/bin/sh -l
 
-echo "::echo::on"
-
-#req="curl -s -o /dev/null -w '%{http_code}\n' -X POST $3 --data \"$1\" $2"
-
 req="curl --write-out '%{http_code}\n' --silent --output /dev/null -X POST $3 --data \"$1\" $2"
+response_code=$(eval $req)
 
-echo $req
+echo "::set-output name=response_code::$response_code"
 
-xxx=$(eval $req)
-
-echo "::set-output name=req::$req"
-
-echo "::set-output name=xxx::$xxx"
-
-status_code=$(curl --write-out '%{http_code}\n' --silent --output /dev/null -X POST $3 --data \"\$1\" $2)
-echo "::set-output name=status_code::$status_code"
-
-#if [ $(curl -s -o /dev/null -w "%{http_code}" -X POST $3 --data \"\$1\" $2) -ne 200 ]; then
-#  echo "%{http_code}"
-#  exit 1;
-#fi
+if [ $response_code -ne 200 ]; then
+  echo "ERROR [$response_code]"
+  exit 1;
+fi
